fcf1b4594e0fa24d28ea29256cc85c92694c9679
Alan <alan.papillu@hotmail.com>
2022-05-31 16:26:23 -0300

    proyecto creado

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputTexto = document.querySelector(".textarea-izq");
+
+const mensaje = document.querySelector(".textarea-der");
+//mensaje.onclick = document.getElementById("rectan").style.display = "none";
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    document.getElementById("rectan").style.display = "none"
+}
+
+function encriptar(stringParaEncriptar){
+
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringParaEncriptar = stringParaEncriptar.toLowerCase();
+    inputTexto.value = "";
+    for (let i = 0; i< matrizCodigo.length; i++){
+        if(stringParaEncriptar.includes(matrizCodigo[i][0])){
+            stringParaEncriptar = stringParaEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringParaEncriptar;
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(inputTexto.value);
+    mensaje.value = textoDesencriptado;
+}
+
+function desencriptar(stringParaDesencriptar){
+
+    let matrizCodigo = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]]
+    stringParaDesencriptar = stringParaDesencriptar.toLowerCase();
+    inputTexto.value = "";
+    for (let i = 0; i< matrizCodigo.length; i++){
+        if(stringParaDesencriptar.includes(matrizCodigo[i][0])){
+            stringParaDesencriptar = stringParaDesencriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringParaDesencriptar;
+}
+
+function areaOcultar(){
+    document.getElementById("rectan").style.display = "none"
+}
+
+function btnCopiar(){
+    var contenido = document.getElementById("textoEncriptado");
+
+    contenido.select();
+    document.execCommand("copy");
+}
